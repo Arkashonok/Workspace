67bf38a312e9e3b3af295ffbca0ef7644a6ec2f8
Arkashonok <54922223+Arkashonok@users.noreply.github.com>
2019-09-30 17:47:16 -0400

    Update SchemeCode.scm

--- Assignment 5/SchemeCode.scm
+++ Assignment 5/SchemeCode.scm
@@ -1,8 +1,10 @@
-
+; Implement a function to check if a list is a palindrome.
 (define palindrom?
   (lambda (li)
     (equal? li (reverse li))))
 
+
+; Merge two sorted lists into one sorted list
 (define list '(1 2 3))
 
 (define list2 '(2 4 6))
